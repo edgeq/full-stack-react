bfb163cb335e364bc3a184551c4772a594e22bc6
edgeq <edgarq86@gmail.com>
2019-08-16 15:24:08 -0500

    set up api routes

---
+++ routes/api/users.js
@@ -0,0 +1,11 @@
+// Handles registering users
+const express = require("express");
+const router = express.Router();
+/**
+ *  @route    GET api/users
+ *  @desc     Test route
+ *  @access   Public
+ */
+router.get("/", (req, res) => res.send("User route"));
+
+module.exports = router;
