18f09ea0f54204427a5ecfffe3b42872ec577b0f
edgeq <edgarq86@gmail.com>
2019-08-21 12:13:15 -0500

    got basic login and register pages working

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+// import axios from "axios";
 
 const Login = () => {
-  return <div>Login</div>;
+  // useState allows for an object with multiple values...
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+  const { email, password } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <>
+      <h1 className="large text-primary">Login</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Sign in to your Account
+      </p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={e => onChange(e)}
+          />
+        </div>
+
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Need an account? <Link to="/register">Register </Link>
+      </p>
+    </>
+  );
 };
 
 export default Login;
