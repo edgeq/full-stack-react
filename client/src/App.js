bb18cb9746a321fbef7a48c8d6a1c55700937d1f
edgeq <edgarq86@gmail.com>
2019-08-21 10:58:39 -0500

    initial components and layouts

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
 
 const App = () => (
   <>
-    <h1>APP GOES HERE</h1>
+    <Navbar />
+    <Landing />
   </>
 );
 
