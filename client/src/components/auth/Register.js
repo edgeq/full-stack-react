ba1bd66d90600c62ab35164596a4aae1b3f644f7
edgeq <edgarq86@gmail.com>
2019-11-06 15:08:36 -0600

    caught up with a basic understanding of redux store, actions, and reducers

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
 
 // import axios from "axios";
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   // useState allows for an object with multiple values...
   const [formData, setFormData] = useState({
     name: "",
@@ -20,7 +23,7 @@ const Register = () => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("passwords no match");
+      setAlert("passwords no match", "danger");
     } else {
       console.log(formData);
       // WITHOUT REDUX
@@ -107,4 +110,11 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { setAlert }
+)(Register);
